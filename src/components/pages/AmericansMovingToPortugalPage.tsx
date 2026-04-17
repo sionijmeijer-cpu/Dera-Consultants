31520f56eeeda30e1fe60bafb921d13c48d29e46
Sylvia Awoudu <sionijmeijer@gmail.com>
2026-04-17 11:09:11 +0100

    Fix import path for OtherProgrammes component

--- src/components/pages/AmericansMovingToPortugalPage.tsx
+++ src/components/pages/AmericansMovingToPortugalPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { blogPosts } from '../../data/blogPosts';
-import OtherProgrammes from './OtherProgrammes';
+import OtherProgrammes from '../OtherProgrammes';
 
 interface AmericansMovingToPortugalPageProps {
   onScheduleCall?: () => void;
